dc9da8eda43e92ab3450ac4bb66704e1f51fdab2
reowoon <65953150+reowoon@users.noreply.github.com>
2020-05-28 15:23:05 +0900

    Create romeda_egg.py

---
+++ romeda_egg.py
@@ -0,0 +1,16 @@
+import asyncio
+from discord.ext import commands
+import cogs
+
+
+class SKYLINE(commands.Bot):
+
+    def __init__(self, **options):
+        super().__init__('egg!', **options)
+        cogs.cogs(self)
+
+    async def close(self):
+        futs = self.extra_events.get('on_close', [])
+        if futs:
+            await asyncio.wait([f() for f in futs])
+        await super().close()
