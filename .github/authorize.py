5f2a46f93785bb3bb7adf8ada5371fef7939757b
reowoon <65953150+reowoon@users.noreply.github.com>
2020-05-28 14:48:39 +0900

    Create authorize.py

---
+++ .github/authorize.py
@@ -0,0 +1,58 @@
+import random
+
+import discord
+from discord.ext import commands
+
+from .general import (
+    ANDROMEDA_ID,
+    chaos_authorize_message,
+    piece_authorize_message,
+    authorize_message_channel,
+    chaos_roles_id,
+    piece_roles_id,
+    chaos_channel,
+    piece_channel
+)
+
+
+class Authorize(commands.Cog):
+
+    def __init__(self, bot, name=None):
+        self.bot: commands.Bot = bot
+        self.name = name if name is not None else type(self).__name__
+
+    def get_member(self, _id):
+        return self.bot.get_guild(ANDROMEDA_ID).get_member(_id)
+
+    @property
+    def default_roles(self):
+        guild = self.bot.get_guild(ANDROMEDA_ID)
+        return [guild.get_role(i) for i in default_roles_id]rgrgrgrgrgggrr
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+        if (
+            payload.channel_id != authorize_message_channel
+            or payload.message_id != piece_authorize_message
+            or payload.emoji.name != "\U0001F33F"
+        ):
+            return
+        member = self.get_member(payload.user_id)
+        if self.default_roles[0] in member.roles:
+            return
+        await member.add_roles(*self.default_roles)
+        name = member.display_name
+        des1 = format(name, member.guild.me.display_name)
+        embed = discord.Embed(
+            title='{0}さんが参加しました。'.format(name),
+            colour=0x2E2EFE,
+            description=(
+                '```\n{3}\n```\n'
+                '{0}！\n'
+                '{1}。\n'
+                '{2}'
+            ).format(name, member.guild.member_count, member.created_at, des1)
+        )
+        embed.set_thumbnail(url=member.avatar_url)
+
+        await self.bot.get_channel(main_channel).send(embed=embed)
