51dd00d831becd0c2bcc302803d6242e76f4946a
reowoon <65953150+reowoon@users.noreply.github.com>
2020-05-28 15:30:29 +0900

    Update authorize.py

--- .github/authorize.py
+++ .github/authorize.py
@@ -14,6 +14,16 @@ from .general import (
     piece_channel
 )
 
+@client.event
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if client.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
 
 class Authorize(commands.Cog):
 
