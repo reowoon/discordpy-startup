9e0979c13d16762a9f65db4d26686cb18acb7075
reowoon <65953150+reowoon@users.noreply.github.com>
2020-05-28 15:45:55 +0900

    Update romeda_egg.py

--- romeda_egg.py
+++ romeda_egg.py
@@ -1,16 +1,1 @@
-import asyncio
-from discord.ext import commands
-import cogs
 
-
-class ANDROMEDA_EGG(commands.Bot):
-
-    def __init__(self, **options):
-        super().__init__('egg!', **options)
-        cogs.cogs(self)
-
-    async def close(self):
-        futs = self.extra_events.get('on_close', [])
-        if futs:
-            await asyncio.wait([f() for f in futs])
-        await super().close()
