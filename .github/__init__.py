6c45b0d34bf8ebf8f410c7a6a4d57a43c34595e7
reowoon <65953150+reowoon@users.noreply.github.com>
2020-05-28 14:55:06 +0900

    Create __init__.py

---
+++ .github/__init__.py
@@ -0,0 +1,32 @@
+  
+from .authorize import Authorize
+from .board import DiscussionBoard
+from .botowner import Bot_Owner_Command
+from .categoryindex import Category_Index
+from .categoryrecover import Category_recover
+from .events import Events
+from .freecategory import FreeCategory
+from .level import Level
+from .message_count import Message_count
+from .normal import Normal_Command
+from .rolesave import Role_save
+from .staff import Staff_Command
+from .ticket import Ticket
+from .vc import VC
+
+
+def cogs(bot):
+    bot.add_cog(Normal_Command(bot, '普通のコマンド'))
+    bot.add_cog(Category_Index(bot, 'カテゴリインデックス'))
+    bot.add_cog(Bot_Owner_Command(bot, 'BOTオーナー用コマンド'))
+    bot.add_cog(Staff_Command(bot, 'スタッフ用コマンド'))
+    bot.add_cog(FreeCategory(bot, '自由チャンネル編集コマンド'))
+    bot.add_cog(Category_recover(bot, 'カテゴリーリカバリー'))
+    bot.add_cog(Events(bot, '参加・退出通知、VC通知'))
+    bot.add_cog(Level(bot, 'レベル機能'))
+    bot.add_cog(DiscussionBoard(bot, '掲示板機能'))
+    bot.add_cog(Role_save(bot, '役職セーブ'))
+    bot.add_cog(Message_count(bot, 'メッセージカウント'))
+    bot.add_cog(Ticket(bot, 'トラブルチケット'))
+    bot.add_cog(Authorize(bot, "認証機能"))
+    bot.add_cog(VC(bot, "VC機能"))
