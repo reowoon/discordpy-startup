5e9e6ba1d3de70b71a44bc4a33c2770b890665af
reowoon <65953150+reowoon@users.noreply.github.com>
2020-10-23 20:38:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,11 +14,8 @@ async def on_ready(): # botが起動したときに動作する処理
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
+@bot.command()
+async def rrr(ctx):
+    await ctx.send(ctx.content)
 
 bot.run(token)
