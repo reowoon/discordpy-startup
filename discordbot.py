b3b8c49fbbf0cbdad925dcf4152fa68dfe225e0e
reowoon <65953150+reowoon@users.noreply.github.com>
2020-10-23 21:14:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,14 +9,15 @@ token = os.environ['DISCORD_BOT_TOKEN']
 @bot.event
 async def on_ready(): # botが起動したときに動作する処理
     await bot.change_presence(activity=discord.Game(name="&help | ロメダ最強！" , type=1))
-    
+      
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
 async def rrr(ctx, arg1, arg2):
-    channel = bot.get_channel(int(arg1))
-    await channel.send(arg2)
+    cha = bot.get_channel(int(arg1))
+    await cha.send(arg2)
+
 
 bot.run(token)
