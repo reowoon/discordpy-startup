35810c38479626f994441ea9f93de6ac0c35d422
reowoon <65953150+reowoon@users.noreply.github.com>
2020-10-23 20:56:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,8 +15,8 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def rrr(ctx, arg1,arg2):
+async def rrr(ctx, arg1, arg2):
     channel = get_channel(arg1)
-    await ctx.channel.send(arg2)
+    await channel.send(arg2)
 
 bot.run(token)
