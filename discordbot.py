dabe3ac79730619ab30b7bde36265945a823f368
reowoon <65953150+reowoon@users.noreply.github.com>
2020-10-24 16:39:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,14 +22,6 @@ async def rrr(ctx, arg1, arg2):
     cha = bot.get_channel(int(arg1))
     await cha.send(arg2)
 
-#アウト
-@bot.command()
-async def out(ctx, arg):
-    if message.author.id != 540536805099831299:
-        return
-    else:
-    role = guild.get_role(769452198588579850)  
-    await arg.add_roles(role)
-    await ctx.send(arg.mention+'が脱落！乙！')
+
     
 bot.run(token)
