7282e34f905adb2eac6b73b0a07085ffd0045a99
reowoon <65953150+reowoon@users.noreply.github.com>
2020-10-28 18:56:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 bot = commands.Bot(command_prefix='&')
 token = os.environ['DISCORD_BOT_TOKEN']
+guild = bot.get_guild(417245684656373766)
 
 #ステータス
 @bot.event
@@ -21,8 +22,8 @@ async def ping(ctx):
 #話す
 @bot.command()
 async def rrr(ctx, arg1, arg2):
-    cha = bot.get_channel(int(arg1))
-    await cha.send(arg2)
+    channel = bot.get_channel(int(arg1))
+    await channel.send(arg2)
 
 #アウト
 @bot.command()
@@ -30,10 +31,14 @@ async def out(ctx, arg:discord.Member):
     if ctx.author.id != 540536805099831299:
         await ctx.send('君には使えないよ！')
         return 
-    guild = bot.get_guild(417245684656373766)
     role = guild.get_role(769452198588579850)  
     await arg.add_roles(role)
     await ctx.send(arg.mention+'が脱落！乙！')
+    
+#ニックネーム
+@bot.command()
+async def name(ctx, arg):
+    await guild.members.edit(nick=arg)
     
 # 時報
 @tasks.loop(seconds=60)
