a16f32a0cbcff8d0243faae45ec3687a5eac8b55
reowoon <65953150+reowoon@users.noreply.github.com>
2020-11-01 14:35:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -45,6 +45,9 @@ async def nick(ctx, arg1:discord.Member, arg2):
     if arg1.id == 714776261410553907:
         await ctx.send('やめてください！')
         return
+    if arg1.id == 540536805099831299:
+        await ctx.send('変えられません！')
+        return
     await ctx.send(arg1.name+'の名前を'+arg2+'にしました！')
     await arg1.edit(nick=arg2)
     
