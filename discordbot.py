79cc64571f75e290037657148816a987498086be
reowoon <65953150+reowoon@users.noreply.github.com>
2020-11-14 18:45:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -45,12 +45,13 @@ async def out(ctx, arg:discord.Member):
 @bot.command()
 async def nick_b(ctx):
     role = guild.get_role(777023800746573835)
-    if ctx.author.roles not in 777023800746573835:
+    if ctx.author.roles in 777023800746573835:
+        await ctx.author.remove_roles(role)
+        await ctx.send('改名拒否役職を外しました！')
+    else:
         await ctx.author.add_roles(role)
         await ctx.send('改名拒否役職を与えました！')
         return
-    await ctx.author.remove_roles(role)
-    await ctx.send('改名拒否役職を外しました！')
 
 
     
