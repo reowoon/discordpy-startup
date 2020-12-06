41242f0c7f34dc3de71f94d666ba72a0772035bb
reowoon <65953150+reowoon@users.noreply.github.com>
2020-12-06 19:08:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -93,10 +93,10 @@ async def on_voice_state_update(member, before, after):
         # ボイスチャンネルに誰もいなくなった場合
         if len(before.channel.members) == 0:
             # テキストチャンネルを削除する
-        await _channel_delete(member, before.channel)
-    else:
+            await _channel_delete(member, before.channel)
+        else:
             # テキストチャンネルから退出させる
-        await _channel_exit(member, before.channel)
+            await _channel_exit(member, before.channel)
 
     # ボイスチャンネルに参加してきた場合
     if after.channel is not None:
